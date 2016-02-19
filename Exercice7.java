607dca3d64d6ba172e7c7fa767798e6975e4536b
Loïc Humbert <loic.humbert@epfl.ch>
2016-02-19 14:52:10 +0100

    Try with hard links

--- Exercice7.java
+++ Exercice7.java
@@ -1,1 +1,24 @@
-/home/loic/NetBeansProjects/Exercice7/src/exercice7/Exercice7.java
+/*
+ * -Lire les valeurs et stocker dans un tableau
+ * -Tri à bulle
+ * -Affichage
+ */
+package exercice7;
+import java.util.*;
+import MyDojoTools.outils;
+
+public class Exercice7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int tableau []= new int [5];
+        for (int i = 0; i < tableau.length; i++) {
+            tableau [i] = outils.askByte("Entrer un byte entre 0 et 127: ");
+        }
+        tableau = outils.bubbleSortIntArray(tableau);
+        System.out.println("Voici votre tableau trié: "+Arrays.toString(tableau));
+    }
+}
+    
--- outils.java
+++ outils.java
@@ -1,1 +1,79 @@
-/home/loic/NetBeansProjects/Exercice7/src/exercice7/outils.java
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercice7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author loic
+ */
+public class outils {
+     
+    public static String ask(String question){
+        System.out.println(question);
+        Scanner scan = new Scanner(System.in);
+        String result = scan.nextLine();
+        
+        return result;
+    }
+    public static double askDouble(String question){
+        double value;
+        try{
+            value = Double.parseDouble(ask(question));
+        }
+        catch(Exception e){
+            value = askDouble("Entrer un nombre!");
+        }
+        return value;
+    }
+    
+    public static int askInt(String question){
+        int value;
+        try{
+            value = Integer.parseInt(ask(question));
+        }
+        catch(Exception e){
+            value = askInt("Entrer un nombre!");
+        }
+        return value;
+    }
+    
+    public static Byte askByte(String question){
+        Byte monByte = (byte) askIntBetween(question, 0, 127);
+        return monByte;
+    }
+    
+    public static int askIntBetween(String question, int min, int max){
+        int monInt = askInt(question); 
+        if (monInt <= max && monInt >= min) {
+            return monInt;
+        } else {
+            return askIntBetween("On a dit entre "+min+" et "+max+"!!!", min, max);
+        }
+    }
+    
+    public static int[] bubbleSortIntArray(int[] tableau){
+        int i = 0;
+	Boolean changed = true;
+	while (i < tableau.length- 1 && changed)
+	{
+		changed = false;
+		for (int index = 0;  index < tableau.length-1; index++)
+		{
+			if (tableau[index] > tableau[index+1])
+			{ 
+				changed = true;
+				int temp = tableau[index];
+				tableau[index] = tableau[index + 1];
+				tableau[index+1] = temp;
+			}
+		}
+		i++;
+	}
+        return tableau;
+    }
+}
